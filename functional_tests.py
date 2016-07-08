e15d880c88236053c7ad10d31e1487ef7dca7cb3
Stefan <sbrandegv@yahoo.com>
2016-07-07 22:20:19 -0400

    fixed up functional_tests

--- functional_tests.py
+++ functional_tests.py
@@ -7,13 +7,9 @@ firefox_capabilities = DesiredCapabilities.FIREFOX
 firefox_capabilities['marionette'] = True
 firefox_capabilities['binary'] = '/usr/bin/firefox'
 
-#display = Display(visible=0, size=(1024, 768))
-#display.start()
-
 browser = webdriver.Firefox(capabilities=firefox_capabilities)
 browser.get('http://localhost:8000')
 
 assert 'Django' in browser.title
 
 browser.quit()
-#display.stop()
