49d187a04f227df21d84a041bbce55b3a15c4c97
Stefan <sbrandegv@yahoo.com>
2016-07-16 20:08:19 -0400

    refactored test of html to use render_to_string of template

--- map/views.py
+++ map/views.py
@@ -1,5 +1,4 @@
 from django.shortcuts import render
-from django.http import HttpResponse
 
 def home_page(request):
-    return HttpResponse("<html><title>I'm Hungry!</title></html>")
+    return render(request, 'home.html')
