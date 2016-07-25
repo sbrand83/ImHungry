449cd99f7749da6681ed5da24b1ed198ef195a9b
Stefan <sbrandegv@yahoo.com>
2016-07-24 23:26:50 -0400

    map page redirects home if invalid get parameters

--- map/tests.py
+++ map/tests.py
@@ -36,3 +36,41 @@ class MapPageTest(TestCase):
 
         self.assertContains(response, 'lat={lat}'.format(lat=fake_lat))
         self.assertContains(response, 'lng={lng}'.format(lng=fake_lng))
+
+    def test_redirect_to_home_if_missing_lat_or_lng(self):
+        client = self.client
+
+        # with no GET parameters, should redirect back home
+        response = client.get('/map')
+        self.assertRedirects(response, '/')
+
+        # missing lng
+        response = client.get('/map', {'lat': 20})
+        self.assertRedirects(response, '/')
+
+        # missing lat
+        response = client.get('/map', {'lng': 20})
+        self.assertRedirects(response, '/')
+    def test_redirect_if_coordinates_invalid(self):
+        client = self.client
+
+        # lat invalid if abs(lat) > 90
+
+        # lat > 90
+        response = client.get('/map', {'lat': 90.1, 'lng': 50})
+        self.assertRedirects(response, '/')
+
+        # lat < -90
+        response = client.get('/map', {'lat': -90.1, 'lng': 50})
+        self.assertRedirects(response, '/')
+
+
+        # lng invalid if abs(lng) > 180
+
+        # lng > 180
+        response = client.get('/map', {'lat': 20, 'lng': 180.1})
+        self.assertRedirects(response, '/')
+
+        # lng < -180
+        response = client.get('/map', {'lat': 20, 'lng': -180.1})
+        self.assertRedirects(response, '/')
--- map/views.py
+++ map/views.py
@@ -1,11 +1,17 @@
-from django.shortcuts import render
+from django.shortcuts import redirect, render
 
 def home_page(request):
     return render(request, 'home.html')
 
 def map(request):
     # TODO: handle case where lat/lng not supplied
+    lat = float(request.GET.get('lat', '200'))
+    lng = float(request.GET.get('lng', '200'))
+
+    if abs(lat) > 90 or abs(lng) > 180:
+        return redirect('/')
+
     return render(request, 'map.html', {
-        'lat': request.GET['lat'],
-        'lng': request.GET['lng'],
+        'lat': lat,
+        'lng': lng
     })
